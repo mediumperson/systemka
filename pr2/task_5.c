39a31a2d4f6a1795d166b96ac88f2bbc8b837e10
mediumperson <eugen.the.fast@gmail.com>
2025-10-13 10:23:02 +0300

    Create task_5.c

---
+++ pr2/task_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file;
+    char filename[100];
+    int number;
+    int count = 0;
+
+    printf("Введите имя файла: ");
+    scanf("%s", filename);
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+
+    while (fscanf(file, "%d", &number) == 1) {
+        if (number > 0 && number % 2 == 0) {
+            count++;
+        }
+    }
+
+    printf("Количество четных положительных чисел: %d\n", count);
+
+    fclose(file);
+    return 0;
+}
